992aac2e0163463f40911a95aee69262275eeb5e
Francis Ferri <49722697+Francis-Ferri-personal@users.noreply.github.com>
2024-06-22 16:27:09 -0600

    Decision tree classification in R

--- Part 3 - Classification/12 - Decision Tree Classification/desicion_tree_classifcation.R
+++ Part 3 - Classification/12 - Decision Tree Classification/desicion_tree_classifcation.R
@@ -1,4 +1,4 @@
-# Logistic Regression
+# Decision Tree Classification
 
 
 # Import dataset
@@ -9,7 +9,7 @@ dataset = read.csv("Social_Network_Ads.csv")
 dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
 
 # install.packages('caTools')
-
+  
 # Spliting the datasetset into Training set and Test set
 library(caTools)
 set.seed(123)
@@ -19,16 +19,18 @@ split = sample.split(dataset$Purchased, SplitRatio = 0.75)
 training_set = subset(dataset, split==TRUE)
 test_set = subset(dataset, split==FALSE)
 
+# NOTE: Feature scaling is optional for this algorithm
 # Feature Scaling
 training_set[,1:2] = scale(training_set[,1:2])
 test_set[,1:2] = scale(test_set[,1:2])
 
 # Fitting Classifier to the training set
-# Create our classifier here
+# install.packages('rpart')
+library(rpart)
+classifier = rpart(formula = Purchased ~ ., data = training_set)
 
 # Predicting the Test set results
-prob_pred = predict(classifier, type='response', newdata = test_set[-3])
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
+y_pred = predict(classifier, newdata = test_set[-3], type = 'class')
 
 # Making the confusion matrix
 cm = table(test_set[,3], y_pred)
@@ -42,10 +44,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+y_grid = predict(classifier, type = 'class', newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Training set)',
+     main = 'Decision Tree (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -59,12 +60,15 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+y_grid = predict(classifier, type = 'class', newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Test set)',
+     main = 'Decision Tree (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
+
+# Plotting the desicion tree
+plot(classifier)
+text(classifier)
