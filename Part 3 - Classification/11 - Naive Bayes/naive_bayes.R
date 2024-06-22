23f9e9f98be2ee465a0b5e6ab42720e5841fb82c
Francis Ferri <49722697+Francis-Ferri-personal@users.noreply.github.com>
2024-06-21 23:55:17 -0600

    Naive Bayes in R

--- Part 3 - Classification/11 - Naive Bayes/naive_bayes.R
+++ Part 3 - Classification/11 - Naive Bayes/naive_bayes.R
@@ -1,13 +1,13 @@
-# Logistic Regression
+# Naive Bayes
 
 
 # Import dataset
-dataset = read.csv("Social_Network_Ads.csv")
-# dataset = dataset[, 3:5]
+
+dataset = read.csv("Social_Network_Ads.csv")# dataset = dataset[, 3:5]
 
 # Encoding the target feature as factor
 dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
-+
+
 # install.packages('caTools')
 
 # Spliting the datasetset into Training set and Test set
@@ -23,12 +23,13 @@ test_set = subset(dataset, split==FALSE)
 training_set[,1:2] = scale(training_set[,1:2])
 test_set[,1:2] = scale(test_set[,1:2])
 
-# Fitting Classifier to the training set
-# Create our classifier here
+# Fitting Naive Bayes to the training set
+library(e1071)
+classifier = naiveBayes(x = training_set[-3], y = training_set$Purchased)
 
 # Predicting the Test set results
-prob_pred = predict(classifier, type='response', newdata = test_set[-3])
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
+y_pred = predict(classifier, newdata = test_set[-3])
+
 
 # Making the confusion matrix
 cm = table(test_set[,3], y_pred)
@@ -42,10 +43,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Training set)',
+     main = 'Naive Bayes (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -59,10 +59,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Test set)',
+     main = 'Naive Bayes (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
