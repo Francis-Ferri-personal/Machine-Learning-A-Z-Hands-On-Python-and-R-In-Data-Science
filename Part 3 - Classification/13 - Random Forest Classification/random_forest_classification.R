8bb4eea8586ceab1b0d0494368ff98e67e8e72b2
Francis Ferri <49722697+Francis-Ferri-personal@users.noreply.github.com>
2024-06-29 13:00:01 -0600

    Random forest classification in R

--- Part 3 - Classification/13 - Random Forest Classification/random_forest_classification.R
+++ Part 3 - Classification/13 - Random Forest Classification/random_forest_classification.R
@@ -1,4 +1,4 @@
-# Logistic Regression
+# Random Forest Classification
 
 
 # Import dataset
@@ -23,8 +23,10 @@ test_set = subset(dataset, split==FALSE)
 training_set[,1:2] = scale(training_set[,1:2])
 test_set[,1:2] = scale(test_set[,1:2])
 
-# Fitting Classifier to the training set
-# Create our classifier here
+# Fitting Random Forest Classification to the training set
+# install.packages('randomForest')
+library(randomForest)
+classifier = randomForest(x = training_set[-3], y = training_set$Purchased, ntree=10)
 
 # Predicting the Test set results
 y_pred = predict(classifier, type='response', newdata = test_set[-3])
@@ -43,7 +45,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier, type = 'response', newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Training set)',
+     main = 'Random Forest Classification (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -59,7 +61,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier, type = 'response', newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Test set)',
+     main = 'Random Forest Classification (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
