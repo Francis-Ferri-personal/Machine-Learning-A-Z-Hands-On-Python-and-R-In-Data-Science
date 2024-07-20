0cf4a4747b4951e3efe8d5c932a3bc04ab815d5c
Francis Ferri <49722697+Francis-Ferri-personal@users.noreply.github.com>
2024-07-19 23:14:59 -0600

    LDA in R

---
+++ Part 9 Dimensionality Reduction/23 - Linear Discriminant Analysis (LDA)/lda.R
@@ -0,0 +1,72 @@
+# LDA
+
+# Import dataset
+dataset = read.csv("Wine.csv")
+
+# install.packages('caTools')
+
+# Spliting the datasetset into Training set and Test set
+library(caTools)
+set.seed(123)
+# Split ratio is for training set in this case not for test
+# The column used is y
+split = sample.split(dataset$Customer_Segment, SplitRatio = 0.8)
+training_set = subset(dataset, split==TRUE)
+test_set = subset(dataset, split==FALSE)
+
+# Feature Scaling
+training_set[-14] = scale(training_set[-14])
+test_set[-14] = scale(test_set[-14])
+
+# Applying LDA
+library(MASS)
+lda = lda(formula = Customer_Segment ~ ., data = training_set)
+training_set = as.data.frame(predict(lda, training_set))
+training_set = training_set[c(5,6,1)]
+
+test_set = as.data.frame(predict(lda, test_set))
+test_set = test_set[c(5,6,1)]
+
+# Fitting Logistic Regression to the training set
+library(e1071)
+classifier = svm(formula = class ~ ., data = training_set, type = 'C-classification', kernel='linear')
+
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-3])
+
+# Making the confusion matrix
+cm = table(test_set[,3], y_pred)
+
+# Visualizing the Training set results
+# install.packages("devtools")
+# devtools::install_version("ElemStatLearn", version = "2015.6.26.2", repos = "http://cran.us.r-project.org")
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('x.LD1', 'x.LD2')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'SVM (Training set)',
+     xlab = 'LD1', ylab = 'LD2',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'dodgerblue', ifelse(y_grid == 1, 'deepskyblue', 'salmon')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'dodgerblue3', ifelse(set[, 3] == 1, 'deepskyblue', 'salmon')))
+
+# Visualizing the Training set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('x.LD1', 'x.LD2')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'SVM (Test set)',
+     xlab = 'LD1', ylab = 'LD2',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'dodgerblue', ifelse(y_grid == 1, 'deepskyblue', 'salmon')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'dodgerblue3', ifelse(set[, 3] == 1, 'deepskyblue', 'salmon')))
