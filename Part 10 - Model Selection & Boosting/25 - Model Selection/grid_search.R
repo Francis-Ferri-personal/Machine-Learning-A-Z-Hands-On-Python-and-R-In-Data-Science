443a9d8fea10ed5b9c53267c5b774979638214d1
Francis Ferri <49722697+Francis-Ferri-personal@users.noreply.github.com>
2024-07-20 16:15:45 -0600

    Grid Search in R

---
+++ Part 10 - Model Selection & Boosting/25 - Model Selection/grid_search.R
@@ -0,0 +1,94 @@
+# K-Fold Cross Validation
+
+# Kernel SVM
+
+# Import dataset
+dataset = read.csv("Social_Network_Ads.csv")
+# dataset = dataset[, 3:5]
+
+# install.packages('caTools')
+
+# Spliting the datasetset into Training set and Test set
+library(caTools)
+set.seed(123)
+# Split ratio is for training set in this case not for test
+# The column used is y
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split==TRUE)
+test_set = subset(dataset, split==FALSE)
+
+# Feature Scaling
+training_set[,1:2] = scale(training_set[,1:2])
+test_set[,1:2] = scale(test_set[,1:2])
+
+# Fitting Kernel SVM to the training set
+# install.packages("e1071")
+# library(e1071)
+# classifier = svm(formula = Purchased ~ ., data= training_set, 
+#                  type = 'C-classification', kernel = 'radial')
+# 
+# # Predicting the Test set results
+# y_pred = predict(classifier, type='response', newdata = test_set[-3])
+# 
+# # Making the confusion matrix
+# cm = table(test_set[,3], y_pred)
+# 
+# # Applying k-fold cross validation
+# # install.packages('caret')
+# library(caret)
+# folds = createFolds(training_set$Purchased, k = 10)
+# cv = lapply(folds, function(x){
+#   training_fold = training_set[-x, ] # x is teh idexes of test fold
+#   test_fold = training_set[x, ]
+#   classifier = svm(formula = Purchased ~ ., data= training_fold, 
+#                    type = 'C-classification', kernel = 'radial')
+#   y_pred = predict(classifier, type='response', newdata = test_fold[-3])
+#   cm = table(test_fold[,3], y_pred)
+#   accuracy = (cm[1,1] + cm[2,2]) / (cm[1,1] + cm[1,2] + cm[2,1] + cm[2,2])
+#   return(accuracy)
+# })
+# 
+# accuracy = mean(as.numeric(cv))
+
+# Applying grid search to find the best parameters
+# install.packages('caret')
+library(caret)
+# The caret package has a lot of models. it is very useful
+# Available models: https://topepo.github.io/caret/available-models.html
+classifier = train(form = Purchased ~ ., data = training_set,  method='svmRadial')
+classifier # Select and Control + Enter
+classifier$bestTune
+
+# Visualizing the Training set results
+# install.packages("devtools")
+# devtools::install_version("ElemStatLearn", version = "2015.6.26.2", repos = "http://cran.us.r-project.org")
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, type = 'response', newdata = grid_set)
+plot(set[, -3],
+     main = 'Kernel SVM (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
+
+# Visualizing the Training set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, type = 'response', newdata = grid_set)
+plot(set[, -3],
+     main = 'Kernel SVM (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
